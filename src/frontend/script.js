3e19cba1eece924c4255688207e76af3bfdf9b09
Lucas-0802 <lucasbrito775823@gmail.com>
2023-02-01 21:02:07 -0300

    Atualização do frontend

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -1,6 +1,8 @@
 
 const API_URL = 'http://localhost:8080/lanchonete'
 const cadastroCliente = document.querySelector('#cadastroCliente')
+const cadastroPedido = document.querySelector('#cadastroPedido')
+
 
 cadastroCliente.onsubmit = function(e) {
     e.preventDefault()
@@ -24,10 +26,47 @@ cadastroCliente.onsubmit = function(e) {
         response.json().then(data => {
             if(data.message === 'sucess') {
                 cadastroCliente.reset()
-                alert('Cadastro realizado com sucesso!')
+               alert('Cadastro realizado com sucesso!')
+                redirecionar()
             } else {
                 alert('Ops, ocorreu um erro!!')
             }
         })
     })
 }
+
+function redirecionar() {
+    window.location.href = "pedidos.html"
+}
+
+cadastroPedido.onclick = function() {
+   const realizarPedido = document.querySelector('#realizarPedido')
+
+   realizarPedido.innerHTML = `
+   <br>
+   <div class="pedido">
+       <label>Selecione uma opção:</label>
+       <select name="select">
+       <option value=""></option>
+       <option value="pizza">Pizza</option>
+       <option value="hamburguer">Hamburguer</option>
+       <option value="hotdog">HotDog</option>
+        </select>
+        <input type="text">
+   </div>
+   <br>
+   <div class="pedido">
+       <label>Selecione uma opção:</label>
+       <select name="select">
+       <option value=""></option>
+       <option value="Coca-Cola">Coca-Cola</option>
+       <option value="Guaraná">Guaraná</option>
+       <option value="Pepsi">Pepsi</option>
+        </select>
+        <input type="text">
+   </div>
+   <br>
+   <button id="finalizarPedido" class="btnPedidos">Finalizar Pedido</button>
+   `
+}
+
